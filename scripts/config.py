29f00971987485536a5835bd6910b4dd39a4687a
d-rens <displscord@gmail.com>
2023-06-02 17:17:04 +0200

    am getting there

--- scripts/config.py
+++ scripts/config.py
@@ -7,6 +7,6 @@ def get_week(d=datetime.today()):
 USERCALENDARID = 'primary'
 CURRENT_COURSE_SYMLINK = Path('/home/daniel/current_course').expanduser()
 CURRENT_COURSE_ROOT = CURRENT_COURSE_SYMLINK.resolve()
-CURRENT_COURSE_WATCH_FILE = Path('/home/daniel/courses').resolve()
+CURRENT_COURSE_WATCH_FILE = Path('/tmp/current_course').resolve()
 ROOT = Path('/home/daniel/documents/courses/').expanduser()
 DATE_FORMAT = '%d-%m-%Y'
