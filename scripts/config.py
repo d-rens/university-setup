4c300999682335c4edf856857506d831e900d013
d-rens <displscord@gmail.com>
2023-07-23 16:31:17 +0200

    changed paths

--- scripts/config.py
+++ scripts/config.py
@@ -5,8 +5,8 @@ def get_week(d=datetime.today()):
     return (int(d.strftime("%W")) + 52 - 5) % 52
 
 USERCALENDARID = 'primary'
-CURRENT_COURSE_SYMLINK = Path('/home/daniel/current_course').expanduser()
+CURRENT_COURSE_SYMLINK = Path('/home/daniel/current_course/').expanduser()
 CURRENT_COURSE_ROOT = CURRENT_COURSE_SYMLINK.resolve()
-CURRENT_COURSE_WATCH_FILE = Path('/tmp/current_course').resolve()
-ROOT = Path('/home/daniel/documents/00-09_privat/00_Git/00.01_docs/j1/').expanduser()
+CURRENT_COURSE_WATCH_FILE = Path('/tmp/current_course/').resolve()
+ROOT = Path('/home/daniel/documents/00-09_privat/00-Git/00.01-docs/j2/').expanduser()
 DATE_FORMAT = '%d-%m-%Y'
